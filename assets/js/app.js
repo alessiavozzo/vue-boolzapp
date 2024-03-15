18f9b4cf40d60293d82819ab2a6fc9807556f726
alessiavozzo <a.vozzo95@gmail.com>
2024-03-15 12:40:17 +0100

    clean code: -delete console logs -import autoreplies array from another js file -clean functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
-import {contactList} from "./contactList.js"
+import { contactList } from "./contactList.js"
+import { autoReplies } from "./autoreplies.js"
 
 const DateTime = luxon.DateTime;
 
@@ -8,91 +9,71 @@ createApp({
     data() {
         return {
 
-            autoReplies:[
-                "ok",
-                "grazie",
-                "ti saluta la nonna",
-                "usciamo oggi?",
-                "ho sonno, vado a dormire",
-                "ho le febbre"
-            ],
-
-
-            contactList: contactList,            
-            activeContact: 0,          
+            autoReplies: autoReplies,
+            contactList: contactList,
+            activeContact: 0,
             myMessage: null,
             contactReply: "",
             searchInput: null,
             appear: false,
-            errorMessage: null, 
-            onlineMessage: null,        
+            errorMessage: null,
+            onlineMessage: null,
+            randomNumber: null,
         }
     },
 
-    methods:{
-        changeActiveChat(contact, index){
+    methods: {
+        changeActiveChat(contact, index) {
             this.activeContact = index;
             //elimina i menu a tendina che ci sono aperti al cambio chat
-            contact.messages.forEach(message=>{
+            contact.messages.forEach(message => {
                 if (message.appear) {
                     delete message.appear;
                 }
             })
         },
 
-        sendMessage(){
+        sendMessage() {
             if (this.myMessage.trim().length > 0) {
                 this.contactList[this.activeContact].messages.push({
                     date: this.updateMsgTime(),
                     message: this.myMessage,
                     status: 'sent'
                 });
-                this.myMessage ="";
-                setTimeout(this.autoContactReply, 1000);                
+                this.myMessage = "";
+                setTimeout(this.autoContactReply, 1000);
             }
-            else{
-                this.errorMessage = "Non puoi inviare un messaggio vuoto"
+            else {
+                this.errorMessage = "Non puoi inviare un messaggio vuoto";
                 setTimeout(() => {
                     this.errorMessage = null;
                 }, 1500);
             }
         },
 
-        autoContactReply(){
-            this.autoReplies.forEach((reply,index) =>{
-                index = this.getRandomNumber(0, this.autoReplies.length-1);
-                this.contactReply = this.autoReplies[index]
-            });
+        autoContactReply() {
+            this.randomNumber = this.getRandomNumber(0, this.autoReplies.length - 1);
+            this.contactReply = this.autoReplies[this.randomNumber];
             this.contactList[this.activeContact].messages.push({
                 date: this.updateMsgTime(),
                 message: this.contactReply,
                 status: 'received'
-            });            
+            });
         },
 
         getRandomNumber(min, max) {
-            return Math.floor(Math.random() * (max - min) ) + min;
+            return Math.floor(Math.random() * (max - min)) + min;
         },
 
-        /* searchContact(){
+        searchContact() {
             this.contactList.forEach(contact => {
-                if(contact.name.toLowerCase().startsWith(this.searchInput.toLowerCase())){
-                    contact.visible = true
-                }
-                else{
-                    contact.visible = false
-                }
-            });
-        }, */
-
-        searchContact(){
-            this.contactList.forEach(contact => {contact.name.toLowerCase().startsWith(this.searchInput.toLowerCase()) ? contact.visible = true : contact.visible = false
-                }
+                contact.name.toLowerCase().startsWith(this.searchInput.toLowerCase()) ? contact.visible = true : contact.visible = false;
+            }
             );
         },
 
-        dropMenuAppear(singleMessage, msgIndex){
-            this.contactList[this.activeContact].messages.forEach((message,index)=> {
+        dropMenuAppear(singleMessage, msgIndex) {
+            this.contactList[this.activeContact].messages.forEach((message, index) => {
                 if (message.appear && msgIndex !== index) {
                     delete message.appear;
                 }
@@ -100,21 +81,20 @@ createApp({
             singleMessage.appear = !singleMessage.appear;
         },
 
-        deleteMessage(msgIndex){
+        deleteMessage(msgIndex) {
             this.contactList[this.activeContact].messages.splice(msgIndex, 1);
-                
         },
 
-        formatMsgTime(time){
+        formatMsgTime(time) {
             return DateTime.fromFormat(time, 'dd/MM/yyyy HH:mm:ss').toFormat('HH:mm');
         },
 
-        updateMsgTime(){
+        updateMsgTime() {
             return DateTime.now().toFormat('dd/MM/yyyy HH:mm:ss');
         }
     },
 
-    mounted(){
-           
+    mounted() {
+
     }
 }).mount('#app')
---
+++ assets/js/autoreplies.js
@@ -0,0 +1,13 @@
+export const autoReplies =[
+    "ok",
+    "grazie",
+    "oggi sono stanca",
+    "ti saluta la nonna",
+    "usciamo oggi?",
+    "ho sonno, vado a dormire",
+    "ho le febbre",
+    "ti va il pollo stasera?",
+    "prova a dire 'tre tigri contro tre tigri'",
+    "prova a dire 'li vuoi quei kiwi?' per 5 volte",
+    "mi sono rotta una gamba"
+]
